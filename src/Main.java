f5a0bfeb66efdf7e846465b1f6c1ac39a683cffb
Chris <cjrumble@yahoo.com>
2021-02-17 18:01:17 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(LastDigitChecker.hasSameLastDigit(23,23,23));
+        System.out.println(LastDigitChecker.hasSameLastDigit(21,22,23));
+        System.out.println(LastDigitChecker.hasSameLastDigit(21,31,41));
+        System.out.println(LastDigitChecker.hasSameLastDigit(44,55,64));
+        System.out.println(LastDigitChecker.hasSameLastDigit(44,55,66));
+        System.out.println(LastDigitChecker.hasSameLastDigit(11,21,91));
+        System.out.println(LastDigitChecker.hasSameLastDigit(3,33,33));
+        System.out.println(LastDigitChecker.hasSameLastDigit(10,11,100));
+
+    }
+
+}
